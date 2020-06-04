c307e642224cc8e1088f96df1507f492463b6974
SteelHorse17 <adrianmagana1792@gmail.com>
2020-06-04 00:41:17 -0500

    home.js

---
+++ home.js
@@ -0,0 +1,22 @@
+console.log('hello');
+// alert('yo'); 
+
+
+// How to write a comment inline
+
+// Varialbles 
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+//var age = prompt('What is your age?');
+
+//document.getElementById('someText').innerHTML = age;
+
+// Numbers in Javascript
+var num1 = 10;
+num1 = num1 + 1; // num1++; is the same thing Increment num1 by 1
+
+console.log(num1);
